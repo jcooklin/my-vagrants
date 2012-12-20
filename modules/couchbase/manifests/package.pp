524ff44376a1c6cbd7ad37639c91c1c1055f9c3c
unknown <jrcookli@JRCOOKLI-DESK.amr.corp.intel.com>
2012-12-19 17:33:58 -0700

    wip

--- modules/couchbase/manifests/package.pp
+++ modules/couchbase/manifests/package.pp
@@ -1,8 +1,11 @@
 class couchbase::package {
 
+  exec {'couchbase-wget':
+    command => '(cd /tmp && wget http://packages.couchbase.com/releases/2.0.0/couchbase-server-community_x86_64_2.0.0.deb)',
+  } ->
   package { 'couchbase':
     ensure => installed,
-    source => "http://packages.couchbase.com/releases/2.0.0/couchbase-server-community_x86_64_2.0.0.deb",
+    source => "/tmp/couchbase-server-community_x86_64_2.0.0.deb",
   }
 
 }
