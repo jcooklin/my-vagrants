4ecb50b1b08ea9ec5001f7f6120ba0ff699ce34c
unknown <jrcookli@JRCOOKLI-DESK.amr.corp.intel.com>
2012-12-20 10:41:46 -0700

    wip

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -12,12 +12,12 @@ class base {
 	content => template('base/apt.conf.erb'),
   } ~>
   exec {"add proxy to global exports":
-    command => 'echo "export http_proxy=http://proxy-us.intel.com:911" >> /etc/environment; echo "export https_proxy=http://proxy-us.intel.com:911" >> /etc/environment; echo "no_proxy=127.0.0.1,localhost,${ipaddress_eth1}" >> /etc/environment',
+    command => 'echo "export http_proxy=http://proxy-us.intel.com:911" >> /etc/environment; echo "export https_proxy=http://proxy-us.intel.com:911" >> /etc/environment; echo "no_proxy=127.0.0.1,localhost,10.0.0.0/8,${ipaddress_eth1}" >> /etc/environment',
     refreshonly => true,
   } ~>
   exec {"update apt":
-    #command => "apt-get -y update;apt-get -y dist-upgrade",
-    command => "apt-get -y update",
+    command => "apt-get -y update;apt-get -y dist-upgrade",
+    #command => "apt-get -y update",
     refreshonly => true,
     timeout => 600,
   }
--- modules/couchbase/manifests/package.pp
+++ modules/couchbase/manifests/package.pp
@@ -3,6 +3,7 @@ class couchbase::package {
   exec {'couchbase-wget':
     provider => shell,
     command => '(cd /tmp && wget http://packages.couchbase.com/releases/2.0.0/couchbase-server-community_x86_64_2.0.0.deb)',
+    environment => ["http_proxy=http://proxy-us.intel.com:911","https_proxy=http://proxy-us.intel.com:911"],
     timeout => 600,
   } ->
   package { 'couchbase':
